8d19ae18dc950afe778ab03afa4c77f39c1ccfe5
Soham Saha <83546205+Sypher0Dronzer@users.noreply.github.com>
2025-08-10 16:20:36 +0530

    s

--- config/connectionDb.js
+++ config/connectionDb.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 import { ENV_VARS } from './envVars.js';
 
 const connectDB = async () => {
-  const mongoLink= ENV_VARS.ENVIROMENT == "dev"? ENV_VARS.MONGO_LOCALDB : ENV_VARS.MONGO_URI
+  // const mongoLink= ENV_VARS.ENVIROMENT == "dev"? ENV_VARS.MONGO_LOCALDB : ENV_VARS.MONGO_URI
+  const mongoLink=  ENV_VARS.MONGO_URI
   try {
     const conn = await mongoose.connect(mongoLink);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
