304ffd4a6b7f696bfed500f6c7e5b417f48c5f10
Soham Saha <83546205+Sypher0Dronzer@users.noreply.github.com>
2025-08-10 17:01:10 +0530

    s

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const app = express();
 app.use(cors({
   origin: ENV_VARS.FRONTEND_LINK,
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  methods: ["GET", "POST", ],
+  allowedHeaders: ["Content-Type", "Authorization","Headers"]
 }));
 
 // ✅ Handle preflight for all routes
