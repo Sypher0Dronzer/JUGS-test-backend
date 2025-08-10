89c0b2e167a7ccefa6f99a02864e7b54eb8ff5d4
Soham Saha <83546205+Sypher0Dronzer@users.noreply.github.com>
2025-08-10 16:54:36 +0530

    s

--- index.js
+++ index.js
@@ -28,7 +28,13 @@ app.options("*", cors({
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", ENV_VARS.FRONTEND_LINK);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 
 app.use(passport.initialize());
